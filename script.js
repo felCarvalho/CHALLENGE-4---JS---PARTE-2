33ed81beca0d65beb68ecad0eb04ccfb9f394e93
felCarvalho <130259846+felCarvalho@users.noreply.github.com>
2024-08-09 15:14:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+//FUNDAMENTOS-JS-PARTE-2 
+//CHALLENGE#4
+//dadosArray = 22, 295, 176, 440, 37, 105, 10, 1100, 86 e 52.
+//porcentagem
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const percentual = [.15, .20];
+const tips = [];
+const totals = [];
+
+const calTip = (contas, porcentagem) => {
+    const percentual1 = porcentagem[0];
+    const percentual2 = porcentagem[1]
+   // console.log(percentual1, percentual2)
+   for (let i = 0; i < contas.length; i++) {
+        if (contas[i] >= 50 && contas[i] <= 300 && percentual1 === .15) {
+        const gorgeta = percentual1 * contas[i];
+        const total = gorgeta + contas[i];
+        tips.push(gorgeta);
+        totals.push(total);
+        //console.log(contas[i], gorgeta, total)
+
+        console.log(`${i}     Sua conta está o valor de ${contas[i]}R$, já a gorgeta de 15% ficou com um valor de ${gorgeta}R$, totalizando ao todo cerca ${total}R$`)
+   } else if (contas[i] <= 50 || contas[i] >= 300 && percentual2 === .20) {
+    const gorgeta = percentual2 * contas[i];
+    const total = gorgeta + contas[i];
+    tips.push(gorgeta);
+    totals.push(total);
+   // console.log(contas[i], gorgeta, total)
+
+    console.log(`${i}     Sua conta está o valor de ${contas[i]}R$, já a gorgeta de 20% ficou com um valor de ${gorgeta}R$, totalizando ao todo cerca ${total}R$`)
+   }
+ }
+};
+const result = calTip(bills, percentual);
+console.log(result);
+//result está retornando 'undefined' pq não estou usando return para o valor produzido pela função voltar para a mesma. E também pq o return mata a repetição do loop.
